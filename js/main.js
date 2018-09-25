833654655c8dbeb83bb8fd71c9b9be2b53a3482f
Greg <greghorne@hotmail.com>
2018-09-25 21:17:54 +0000

    changes to api data retrieval and display

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+
+var gMyControl
+
+function createIncidentTextControl(map, title) {
+
+    if (gMyControl) map.removeControl(gMyControl)
+
+    var textCustomControl = L.Control.extend({
+        options: {
+            position: 'topright' 
+        },
+
+        onAdd: function() {
+            var container;
+
+            container = L.DomUtil.create('div', 'highlight-background custom-control cursor-pointer leaflet-bar', L.DomUtil.get('map'));
+            container.innerHTML = "<center>" + title + "</center>"
+
+            return container;
+        },
+
+        onRemove: function(map) { }
+
+    });
+
+    gMyControl = new textCustomControl();
+    map.addControl(gMyControl);
+}
+
+
 //////////////////////////////////////////////////////////////////////
 // build map layers dynamically from CONST_MAP_LAYERS
 var mapLayers  = [];
@@ -25,24 +55,38 @@ $(document).ready(function() {
         layers: [mapLayers[baseLayer]]
     });
 
-    L.control.layers(baseMaps).addTo(map)                     // add all map layers to layer control
+    // L.control.layers(baseMaps).addTo(map)                      // add all map layers to layer control
     L.control.scale({imperial: true, metric: true}).addTo(map) // add scalebar
 
 
     var timeout
     var coordLatLng
 
+    // credit: https://stackoverflow.com/questions/15066849/how-to-detect-when-mousemove-has-stopped
     map.on('mousemove', function (e) {
         if (timeout !== undefined) {
-            coordLatLng = e.latlng
             window.clearTimeout(timeout);
+            coordLatLng = e.latlng
         }
 
         timeout = window.setTimeout(function (e) {
-            // console.log(coordLatLng)
-            var apiString = "coordinate-info.herokuapp.com/api/v1/coord_info?db=mongo&latitude_y=" + coordLatLng.lat + "&longitude_x=" + coordLatLng.lng
-            console.log(apiString)
-        }, 1000);
+
+            var apiString = "https://coordinate-info.herokuapp.com/api/v1/coord_info?db=mongo&latitude_y=" + coordLatLng.lat + "&longitude_x=" + coordLatLng.lng
+            $.ajax({ type: "GET", url: apiString }).done(function(response){
+                var results = response.results
+                var myText = ""
+                if (response.success == 1) {
+                    myText = results.country + "</br>" + results.municipality1 + "</br>" + results.municipality2
+                    if (results.municipality_nl1 !== null && results.municipality_nl2 !== null) {
+                        myText = results.country + "</br>" + results.municipality1 + " (" + results.municipality_nl1 + ")</br>" + results.municipality2 + " (" + results.municipality_nl2 + ")"
+                    } else {
+                        myText = results.country + "</br>" + results.municipality1 + "</br>" + results.municipality2
+                    }
+                }
+                console.log(response)
+                createIncidentTextControl(map, myText)
+            })
+        }, 500);
     })
 
 })
