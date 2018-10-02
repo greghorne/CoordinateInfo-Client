7119e47dbc4f454904c4253889bb45aa59aad315
Greg <greghorne@hotmail.com>
2018-10-02 21:26:26 +0000

    changed map layer order and removed one map layer

--- js/constants.js
+++ js/constants.js
@@ -7,15 +7,6 @@ const CONST_MAP_INITIAL_ZOOM            =  3;
 // definition of map layers; first layer is the default layer displayed
 const CONST_MAP_LAYERS = [
     {
-        // not https so can generate warnings due to mixed content
-        // 2018-08-12 - https site currently has a NET::ERR_CERT_COMMON_NAME_INVALID
-        name: "Grayscale OSM",
-        url: "http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png",      
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-        minZoom:  5,
-        maxZoom: 17
-    },
-    {
         name: "Esri OSM",
         url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
         attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
@@ -23,9 +14,11 @@ const CONST_MAP_LAYERS = [
         maxZoom: 17
     },
     {
-        name: "Hydda OSM",
-        url: "https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png",
-        attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        // not https so can generate warnings due to mixed content
+        // 2018-08-12 - https site currently has a NET::ERR_CERT_COMMON_NAME_INVALID
+        name: "Grayscale OSM",
+        url: "http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png",      
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
         minZoom:  5,
         maxZoom: 17
     },
